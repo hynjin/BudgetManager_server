b3298cc41e7e7c65fb5b2714114d8db558966e08
hyunjin <hynjin0110@gmail.com>
2024-07-11 18:49:57 -0400

    edit landing page

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,12 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     serverComponentsExternalPackages: ['mongodb'],
+//   },
+// }
+
+// module.exports = nextConfig
