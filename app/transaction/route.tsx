7aab65128fd7960ecfd43979cb13e03d76aa6e6d
hyunjin <hynjin0110@gmail.com>
2024-07-18 01:50:03 -0400

    post transaction

--- app/transaction/route.tsx
+++ app/transaction/route.tsx
@@ -37,7 +37,25 @@ export async function GET(request: Request) {
   }
 }
 
-export async function POST(request: Request) {}
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  if (!body) {
+    return NextResponse.json({ error: "Please provide data" }, { status: 400 });
+  }
+
+  try {
+    const client = await connectToDatabase();
+    const db = client.db("Dev");
+    const collection = db.collection("transactions");
+
+    const result = await collection.insertOne(body);
+
+    return NextResponse.json({ message: "Document inserted", result });
+  } catch (err) {
+    return NextResponse.json({ error: "Server Error" }, { status: 500 });
+  }
+}
 
 export async function PUT(request: Request) {}
 
